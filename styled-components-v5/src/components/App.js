55d1ceea3b597413098e18c6c3c0891531a7302e
skiran017 <skiran017@gmail.com>
2021-03-08 10:08:47 +0530

    GlobalStyle and Button(common)

--- styled-components-v5/src/components/App.js
+++ styled-components-v5/src/components/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Button } from "components/common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: white;
+    min-height: 100vh;
+    margin: 0 ;
+    color: black;
+    font-family: 'Kaushan Script'
+  }
+`; //to apply global styles
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>App</h1>
       <Button>Test</Button>
     </>
